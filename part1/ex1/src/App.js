413d137b1c026a172f64d2dee01cc1a9b19f8c1b
lanuanra <lanuanra@student.jyu.fi>
2021-10-22 13:08:13 +0300

    first exercise

--- part1/ex1/src/App.js
+++ part1/ex1/src/App.js
@@ -1,18 +1,28 @@
 import React from 'react'
 
 const App = () => {
-    const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
 
+    const course = {
+    name: 'Half Stack application development',
+    parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
     const Header = () => {
         return (
             <div>
-                <p>This is {course}. Welcome!</p>
+                <p>This is {course.name}. Welcome!</p>
             </div>
         )
     }
@@ -27,20 +37,29 @@ const App = () => {
     }
 
 
+
     const Content = () => {
+
+        const m1 = course.parts.map(value => 'This is ' + value.name)
+        const m2 = course.parts.map(value => ' and ' + value.exercises + ' exercises have been done')
+
         return (
             <div>
-                <Part part={part1} exercises={exercises1} />
-                <Part part={part2} exercises={exercises2} />
-                <Part part={part3} exercises={exercises3} />
+
+                <p> {m1[0]} {m2[0]} </p>
+                <p> {m1[1]} {m2[1]} </p>
+                <p> {m1[2]} {m2[2]} </p>
             </div>
         )
     }
 
     const Total = () => {
+
+        const m3 = course.parts.map(value => value.exercises )
+
         return (
             <div>
-                <p> Total of {exercises1 + exercises2 + exercises3} have been done.</p>
+                <p> Total of {m3[0] + m3[1] + m3[2]} have been done.</p>
             </div>
         )
     }
@@ -48,8 +67,8 @@ const App = () => {
     return (
         <div>
             <Header course={course} />
-            <Content part1={part1} />
-            <Total exercises1={exercises1} />
+            <Content parts={course} />
+            <Total parts={course} />
         </div>
     )
 
